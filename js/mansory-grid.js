6b274e12f7f214339ca648781f274c50968020de
Vasiliy <dvv03122014@gmail.com>
2024-01-20 15:13:13 +0200

    updated

--- js/mansory-grid.js
+++ js/mansory-grid.js
@@ -8,19 +8,24 @@ function resizeAllGridItems(){
 }
 
 function resizeGridItem(item) {
+    // getting parent element
     const grid = document.getElementsByClassName("gallery")[0];
+
     const rowHeight = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-auto-rows'));
+
     console.log('ROW___HEIGHT________', rowHeight);
     const rowGap = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-row-gap'));
-    let rowSpan;
-    if(!!item.querySelector('.content')) {
-        rowSpan = Math.ceil((item.querySelector('.content').getBoundingClientRect().height+rowGap)/(rowHeight+rowGap));
 
-    } else {
-        console.log('WRONG_____', item)
-    }
+    const content = item.querySelector('.content');
+
+    // getting height of photo element
+    const { height } = content.getBoundingClientRect();
+    const calculated = (height + rowGap) / (rowHeight + rowGap);
+    const rowSpan = Math.ceil(calculated);
+
     console.log('ROW___rowSpan_______', rowSpan);
-    item.style.gridRowEnd = "span "+rowSpan;
+    
+    item.style.gridRowEnd = `span ${rowSpan}`;
 }
 
 window.onload = resizeAllGridItems();
