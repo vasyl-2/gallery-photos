98adb701cc31b64dc7c8314565976b279ff71356
Vasiliy <dvv03122014@gmail.com>
2024-01-20 15:15:46 +0200

    updated

--- js/mansory-grid.js
+++ js/mansory-grid.js
@@ -1,33 +1,33 @@
+function resizeAllGridItems() {
+    const allItems = document.getElementsByClassName('photo');
 
-
-function resizeAllGridItems(){
-    const allItems = document.getElementsByClassName("photo");
-    for(let x=0; x<allItems.length; x++){
+    for(let x = 0; x < allItems.length; x++){
         resizeGridItem(allItems[x]);
     }
 }
 
 function resizeGridItem(item) {
     // getting parent element
-    const grid = document.getElementsByClassName("gallery")[0];
+    const grid = document.getElementsByClassName('gallery')[0];
 
+    // getting grid-auto-rows property of parent element
     const rowHeight = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-auto-rows'));
 
-    console.log('ROW___HEIGHT________', rowHeight);
+    // getting grid-row-gap property of parent element
     const rowGap = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-row-gap'));
 
     const content = item.querySelector('.content');
-
+    
     // getting height of photo element
     const { height } = content.getBoundingClientRect();
     const calculated = (height + rowGap) / (rowHeight + rowGap);
+
     const rowSpan = Math.ceil(calculated);
 
-    console.log('ROW___rowSpan_______', rowSpan);
-    
+    // set grid-row-end property on photo element
     item.style.gridRowEnd = `span ${rowSpan}`;
 }
 
 window.onload = resizeAllGridItems();
-window.addEventListener("resize", resizeAllGridItems);
+window.addEventListener('resize', resizeAllGridItems);
 
